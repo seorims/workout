be39238d19fe98132d5e0936a329e61bba0256ec
nico alavi <alavinic@gmail.com>
2025-01-23 20:28:09 +0900

    correct active record error in workoutsessions.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,17 @@
 class User < ApplicationRecord
-
   # trainer relationship
   has_many :workout_sessions, foreign_key: "user_id", class_name: "WorkoutSession"
 
   # trainee relationship (via bookings)
   has_many :bookings
   has_many :attended_workouts, through: :bookings, source: :workout_session
-   # include default devise modules. note to self, others are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  # devise modules
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def avatar_url
+    # uses ui-avatars api as fallback for all users
+    "https://ui-avatars.com/api/?name=#{name || 'User'}"
+  end
 end

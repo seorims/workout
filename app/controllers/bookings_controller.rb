2719929a8be3bc37636978662bc5d9d66c05a62a
Lio Catalan <182225969+LioCat-hub@users.noreply.github.com>
2025-01-25 12:45:11 +0900

    Add Book Now! button in one session And Redirecting path after sign in

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,23 +1,24 @@
 class BookingsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :create]
+  # Ensure users are authenticated before creating a booking
+  before_action :authenticate_user!, only: [:create]
 
   def create
     @workout_session = WorkoutSession.find(params[:workout_session_id])
-    @booking = current_user&.bookings&.new(
+    @booking = current_user.bookings.new(
       workout_session: @workout_session,
       booked_at: Time.now.in_time_zone('Tokyo')
     )
 
-    if @booking&.save
+    if @booking.save
       redirect_to bookings_path, notice: 'Booking successfully created.'
     else
-      Rails.logger.error @booking&.errors&.full_messages&.join(", ")
-      redirect_to workout_session_path(@workout_session), alert: "Failed to create booking: #{@booking&.errors&.full_messages&.join(", ")}"
+      Rails.logger.error @booking.errors.full_messages.join(", ")
+      redirect_to workout_session_path(@workout_session), alert: "Failed to create booking: #{@booking.errors.full_messages.join(", ")}"
     end
   end
 
   def index
-    @bookings = current_user&.bookings&.includes(:workout_session)
+    @bookings = current_user.bookings.includes(:workout_session)
   end
 
   private

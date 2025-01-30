ee18c272664cbdbb255495ae403706d448069907
nico alavi <alavinic@gmail.com>
2025-01-30 19:54:27 +0900

    adjust workout session model.

--- app/models/workout_session.rb
+++ app/models/workout_session.rb
@@ -3,7 +3,11 @@ class WorkoutSession < ApplicationRecord
   has_many :bookings
   has_many :trainees, through: :bookings, source: :user
 
-  # def start_time
-  #   bookings.first&.start_time
-  # end
+  validate :trainer_only_creates_sessions
+
+  private
+
+  def trainer_only_creates_sessions
+    errors.add(:base, "Only trainers can create sessions") unless trainer&.role == 'trainer'
+  end
 end

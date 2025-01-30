40a77ca38f2bb6bda1027b136f435b29c1942f45
nico alavi <alavinic@gmail.com>
2025-01-30 19:54:54 +0900

    adjust booking model.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,4 +5,11 @@ class Booking < ApplicationRecord
   validates :booked_at, presence: true
   validates :status, inclusion: { in: ['pending', 'confirmed', 'cancelled'], message: "%{value} is not a valid status" }, allow_nil: true
   validates :start_time, presence: true
+  validate :client_only_books_sessions
+
+  private
+
+  def client_only_books_sessions
+    errors.add(:base, "Trainers cannot book sessions") if user&.role == 'trainer'
+  end
 end
